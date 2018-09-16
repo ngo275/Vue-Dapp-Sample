23ec4e32937024f7fbceb712ecdd3119c1438b5c
ngo275 <ngo38114@gmail.com>
2018-09-16 12:28:09 +0800

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+ <Metamask/>
+</template>
+<script>
+import Metamask from '@/components/Metamask'
+export default {
+  name: 'Main',
+  beforeCreate() {
+    this.$store.dispatch('registerWeb3')
+  },
+  components: {
+    'Metamask': Metamask
+  }
+}
+</script>
+<style scoped>
+</style>
